e57285d665dbe3a3666a31590194a8f95276d0f1
sosekashish4-spec <sosekashish4@gmail.com>
2026-06-03 20:40:41 +0530

    Time: 3 ms (18.57%), Space: 47.4 MB (95.38%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int i=0;
+        int j=1;
+        while(j<arr.length){
+            if(arr[i]==0 && arr[j]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j++;
+            }else if(arr[i]==0 && arr[j]==0){
+                 j++;
+            }else{
+                i++;
+                j++;
+            }
+        }
+    }
+}
