b1cc42e6d75ebc9ac12928df14efa4f5886a0e29
Kashish <sosekashish4@gmail.com>
2026-06-15 12:43:24 +0530

    Time: 100 ms (5.93%), Space: 46.2 MB (6.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,25 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) return false;
-        long sum=0;
-        int y=x;
-        while(x>0){
-            int m=x%10;
-            sum+=m;
-            sum*=10;
-            x/=10;
-        }
-        if(sum/10==y) return true;
+        // if(x<0) return false;
+        // long sum=0;
+        // int y=x;
+        // while(x>0){
+        //     int m=x%10;
+        //     sum+=m;
+        //     sum*=10;
+        //     x/=10;
+        // }
+        // if(sum/10==y) return true;
+        // return false;
+
+        //Recursion
+        long y=(long)x;
+        if(y<0) return false;
+        if(y==rev(x,0)) return true;
         return false;
+    }
+    public static long rev(long x,long sum){
+        if(x==0) return sum;
+        return rev(x/10,sum*10+x%10);
     }
 }
