d8caeca79141174645d84046e66384834d001f8b
Kashish <sosekashish4@gmail.com>
2026-06-17 14:01:19 +0530

    Time: 0 ms (100%), Space: 48.3 MB (68.36%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,13 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int lo=0;
-        int hi=nums.length-1;
-        while(lo<=hi){
-            int mid=((hi-lo)/2)+lo;
-            if(nums[mid]<target) lo=(mid+1);
-            else if(nums[mid]>target) hi=(mid-1);
-            else return mid;
-        }
-        return -1;
+        // int lo=0;
+        // int hi=nums.length-1;
+        // while(lo<=hi){
+        //     int mid=((hi-lo)/2)+lo;
+        //     if(nums[mid]<target) lo=(mid+1);
+        //     else if(nums[mid]>target) hi=(mid-1);
+        //     else return mid;
+        // }
+        // return -1;
+
+        //Recursion
+        return bin(0,nums.length-1,target,nums);
+    }
+    public static int bin(int lo,int hi,int target,int[] arr){
+        int mid=(hi-lo)/2+lo;
+        if(lo>hi) return -1;
+        if(arr[mid]>target) return bin(lo,mid-1,target,arr);
+        else if(arr[mid]==target) return mid;
+        else return bin(mid+1,hi,target,arr);
     }
 }
