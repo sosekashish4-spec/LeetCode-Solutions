7b6e51c3500dca097acd702a1497ca34a38deda3
Kashish <sosekashish4@gmail.com>
2026-07-24 08:32:46 +0530

    Time: 72 ms (26.35%), Space: 133.9 MB (53.11%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -13,14 +13,10 @@ class Solution {
         Stack<ListNode>st=new Stack<>();
         ListNode temp=head;
         while(temp!=null){
-            if(st.size()==0) st.push(temp);
-            else if(temp.val<=st.peek().val) st.push(temp);
-            else{
                 while(st.size()>0 && st.peek().val<temp.val){
                     st.pop();
                 }
                 st.push(temp);
-            }
             temp=temp.next;
         }
 
